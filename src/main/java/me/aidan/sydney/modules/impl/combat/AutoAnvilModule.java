f5fa05c12fc5ba0a108b35350b3d68effbc87ddf
playstation451 <playstation451>
2026-01-31 04:18:05 +0000

    rotations for auto anvil

--- src/main/java/me/aidan/sydney/modules/impl/combat/AutoAnvilModule.java
+++ src/main/java/me/aidan/sydney/modules/impl/combat/AutoAnvilModule.java
@@ -1,93 +1,92 @@
 package me.aidan.sydney.modules.impl.combat;
 
+import me.aidan.sydney.Sydney;
 import me.aidan.sydney.events.SubscribeEvent;
-import me.aidan.sydney.events.impl.TickEvent;
+import me.aidan.sydney.events.impl.PlayerUpdateEvent;
 import me.aidan.sydney.modules.Module;
 import me.aidan.sydney.modules.RegisterModule;
 import me.aidan.sydney.settings.impl.BooleanSetting;
+import me.aidan.sydney.settings.impl.ModeSetting;
 import me.aidan.sydney.settings.impl.NumberSetting;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.entity.Entity;
+import me.aidan.sydney.utils.minecraft.InventoryUtils;
+import me.aidan.sydney.utils.minecraft.WorldUtils;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
-import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
 import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.MathHelper;
 
 @RegisterModule(name = "AutoAnvil", description = "Places anvils above players at the highest possible position.", category = Module.Category.COMBAT)
 public class AutoAnvilModule extends Module {
     
-    private long lastPlaceTime = 0;
+    public ModeSetting autoSwitch = new ModeSetting("Switch", "The mode that will be used for automatically switching to anvils.", "Silent", InventoryUtils.SWITCH_MODES);
+    public NumberSetting delay = new NumberSetting("Delay", "The amount of ticks that have to be waited for between placements.", "ticks", 0, 0, 20);
+    public BooleanSetting rotate = new BooleanSetting("Rotate", "Sends a packet rotation whenever placing a block.", true);
+    public BooleanSetting strictDirection = new BooleanSetting("StrictDirection", "Only places using directions that face you.", false);
+    public NumberSetting range = new NumberSetting("Range", "Maximum range to target players.", "blocks", 10.0, 1.0, 20.0);
+    public BooleanSetting crystalDestruction = new BooleanSetting("CrystalDestruction", "Destroys any crystals that interfere with block placement.", true);
+    public BooleanSetting render = new BooleanSetting("Render", "Whether or not to render the place position.", true);
+    public BooleanSetting autoDisable = new BooleanSetting("AutoDisable", "Automatically disables if no anvil is found.", true);
     
-    public NumberSetting delay = new NumberSetting("Delay", "Delay between anvil placements in milliseconds.", "ms", 250, 250, 1500);
-    public BooleanSetting rotate = new BooleanSetting("Rotate", "Rotates to the block before placing.", true);
-    public BooleanSetting strictDirection = new BooleanSetting("Strict Direction", "Uses strict direction checking for placement.", false);
-    public NumberSetting range = new NumberSetting("Range", "Maximum range to target players.", "blocks", 10, 1, 20);
-    public BooleanSetting autoDisable = new BooleanSetting("Auto Disable", "Automatically disables if no anvil is found.", true);
-    
-    private Entity target;
+    private PlayerEntity target;
+    private int ticks = 0;
     
     @SubscribeEvent
-    public void onTick(TickEvent event) {
+    public void onPlayerUpdate(PlayerUpdateEvent event) {
         if (mc.player == null || mc.world == null) return;
         
-        int anvilSlot = getHotbarItemSlot(Items.ANVIL);
+        if (ticks < delay.getValue().intValue()) {
+            ticks++;
+            return;
+        }
+        
+        int anvilSlot = InventoryUtils.find(Items.ANVIL, 0, 
+            autoSwitch.getValue().equalsIgnoreCase("AltSwap") || autoSwitch.getValue().equalsIgnoreCase("AltPickup") ? 35 : 8);
+        
         if (anvilSlot == -1) {
             if (autoDisable.getValue()) {
+                Sydney.CHAT_MANAGER.tagged("No anvils could be found in your hotbar.", getName());
                 setToggled(false);
             }
             return;
         }
         
-        target = getClosestPlayer(range.getValue().floatValue());
+        int previousSlot = mc.player.getInventory().selectedSlot;
+        
+        target = getClosestPlayer();
         if (target == null) return;
         
-        if (System.currentTimeMillis() - lastPlaceTime < delay.getValue().longValue()) return;
-        
-        BlockPos placePos = getHighestPlaceablePos((PlayerEntity) target);
+        BlockPos placePos = getHighestPlaceablePos(target);
         if (placePos == null) return;
         
-        Direction direction = getPlaceableSide(placePos);
+        Direction direction = WorldUtils.getDirection(placePos, strictDirection.getValue());
         if (direction == null) return;
         
-        mc.player.getInventory().selectedSlot = anvilSlot;
+        InventoryUtils.switchSlot(autoSwitch.getValue(), anvilSlot, previousSlot);
         
-        if (rotate.getValue()) {
-            float[] rotations = getBlockRotations(placePos, direction);
-            if (rotations != null) {
-                mc.player.setYaw(rotations[0]);
-                mc.player.setPitch(rotations[1]);
-            }
-        }
+        WorldUtils.placeBlock(placePos, direction, Hand.MAIN_HAND, rotate.getValue(), crystalDestruction.getValue(), render.getValue());
         
-        placeBlock(placePos, direction);
-        lastPlaceTime = System.currentTimeMillis();
+        InventoryUtils.switchBack(autoSwitch.getValue(), anvilSlot, previousSlot);
+        
+        ticks = 0;
     }
     
-    private int getHotbarItemSlot(net.minecraft.item.Item item) {
-        for (int i = 0; i < 9; i++) {
-            if (mc.player.getInventory().getStack(i).getItem() == item) {
-                return i;
-            }
-        }
-        return -1;
-    }
-    
-    private Entity getClosestPlayer(float range) {
-        Entity closest = null;
-        double closestDist = range;
+    private PlayerEntity getClosestPlayer() {
+        PlayerEntity closest = null;
+        double closestDist = range.getValue().doubleValue();
         
-        for (Entity entity : mc.world.getEntities()) {
-            if (entity == mc.player) continue;
-            if (!(entity instanceof PlayerEntity)) continue;
+        for (AbstractClientPlayerEntity player : mc.world.getPlayers()) {
+            if (player == mc.player) continue;
+            if (!player.isAlive() || player.getHealth() <= 0.0f) continue;
+            if (Sydney.FRIEND_MANAGER.contains(player.getName().getString())) continue;
             
-            double dist = mc.player.distanceTo(entity);
-            if (dist < closestDist) {
-                closest = entity;
+            double dist = mc.player.squaredDistanceTo(player);
+            if (dist > MathHelper.square(range.getValue().doubleValue())) continue;
+            
+            if (closest == null || dist < closestDist) {
+                closest = player;
                 closestDist = dist;
             }
         }
@@ -98,80 +97,19 @@ public class AutoAnvilModule extends Module {
     private BlockPos getHighestPlaceablePos(PlayerEntity player) {
         BlockPos playerPos = player.getBlockPos();
         
+        // Start from 2 blocks above player's feet and go up to 5 blocks
         for (int y = 2; y <= 5; y++) {
             BlockPos checkPos = playerPos.up(y);
             
+            // Check if position is replaceable and placeable
             if (mc.world.getBlockState(checkPos).isReplaceable() && 
-                canPlaceBlock(checkPos)) {
+                WorldUtils.isPlaceable(checkPos) &&
+                WorldUtils.getDirection(checkPos, strictDirection.getValue()) != null) {
                 return checkPos;
             }
         }
         
         return null;
-    }
-    
-    private boolean canPlaceBlock(BlockPos pos) {
-        if (!mc.world.getBlockState(pos).isReplaceable()) return false;
-        
-        return getPlaceableSide(pos) != null;
-    }
-    
-    private Direction getPlaceableSide(BlockPos pos) {
-        Direction[] directions = strictDirection.getValue() 
-            ? new Direction[]{Direction.DOWN, Direction.UP}
-            : Direction.values();
-        
-        for (Direction dir : directions) {
-            BlockPos neighbor = pos.offset(dir);
-            Block block = mc.world.getBlockState(neighbor).getBlock();
-            
-            if (block != Blocks.AIR && block != Blocks.WATER && block != Blocks.LAVA) {
-                return dir;
-            }
-        }
-        
-        return null;
-    }
-    
-    private float[] getBlockRotations(BlockPos pos, Direction side) {
-        Vec3d eyePos = mc.player.getEyePos();
-        Vec3d blockCenter = Vec3d.of(pos).add(0.5, 0.5, 0.5);
-        Vec3d directionVec = Vec3d.of(side.getVector()).multiply(0.5);
-        Vec3d targetVec = blockCenter.add(directionVec);
-        
-        double diffX = targetVec.x - eyePos.x;
-        double diffY = targetVec.y - eyePos.y;
-        double diffZ = targetVec.z - eyePos.z;
-        
-        double diffXZ = Math.sqrt(diffX * diffX + diffZ * diffZ);
-        
-        float yaw = (float) Math.toDegrees(Math.atan2(diffZ, diffX)) - 90F;
-        float pitch = (float) -Math.toDegrees(Math.atan2(diffY, diffXZ));
-        
-        return new float[]{yaw, pitch};
-    }
-    
-    private void placeBlock(BlockPos pos, Direction side) {
-        BlockPos neighbor = pos.offset(side);
-        Direction opposite = side.getOpposite();
-        
-        Vec3d hitVec = Vec3d.of(neighbor).add(0.5, 0.5, 0.5)
-            .add(Vec3d.of(opposite.getVector()).multiply(0.5));
-        
-        BlockHitResult hitResult = new BlockHitResult(
-            hitVec,
-            opposite,
-            neighbor,
-            false
-        );
-        
-        mc.getNetworkHandler().sendPacket(new PlayerInteractBlockC2SPacket(
-            Hand.MAIN_HAND,
-            hitResult,
-            0
-        ));
-        
-        mc.player.swingHand(Hand.MAIN_HAND);
     }
     
     @Override
